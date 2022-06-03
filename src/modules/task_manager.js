595b4476a84caf461eb1795f5e38e4cd133c5eb0
jevazquezb <je.vazquezb@gmail.com>
2022-06-02 22:08:52 -0500

    Implement adding, deleting, and editing functionalities to the app

---
+++ src/modules/task_manager.js
@@ -0,0 +1,45 @@
+import Task from './task_class.js';
+
+export let tasksList;
+
+export function getTasks() {
+  tasksList = JSON.parse(localStorage.getItem('tasksList')) || [];
+};
+
+export function saveTasks(data = tasksList) {
+  localStorage.setItem('tasksList', JSON.stringify(data));
+};
+
+export function addTask(description) {
+  const taskId = tasksList.length + 1;    
+  const newTask = new Task(description, taskId);
+  tasksList.push(newTask);
+  return newTask;
+};
+
+export function removeFromStorage(e) {
+  getTasks();
+  const taskId = +e.target.id.slice(5);
+  const filteredTasks = tasksList.filter((task) => task.index !== taskId);
+  for (let i = 0; i < filteredTasks.length; i++ ) {
+    filteredTasks[i].index = i + 1;
+  }
+  saveTasks(filteredTasks);
+}
+
+export function removeAllChildElements(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
+
+export function editDescription(e) {
+  getTasks();
+  const taskId = +e.target.id.slice(4);
+  tasksList.forEach(task => {
+    if (taskId === task.index) {
+      task.description = e.target.value;
+    }
+  });
+  saveTasks();
+}
