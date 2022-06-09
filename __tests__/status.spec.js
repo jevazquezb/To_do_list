4ddeecf824ea82baaf5f514dcf0ad09eab776fa5
dqarias <dino.quispea@gmail.com>
2022-06-09 11:07:44 -0500

    Add testing for update status

--- __tests__/status.spec.js
+++ __tests__/status.spec.js
@@ -14,3 +14,14 @@ describe('Editing function tests', () => {
     expect(updatePropertyValue(list, index, property, value)).toEqual(output);
   });
 });
+
+describe('Updating status function tests', () => {
+  test('Change the value of the completed property', () => {
+    const list = [{completed: false, description: 'First message', index: 1}];
+    const index = 1;
+    const property = 'completed';
+    const value = true;
+    const output = [{completed: true, description: 'First message', index: 1}]
+    expect(updatePropertyValue(list, index, property, value)).toEqual(output);
+  });
+});
